2769fe6707e41efe409f544d3906994541a75769
GeekerHua <geekerhua@gmail.com>
2019-06-21 21:21:41 +0800

    update profile and link

--- link.sh
+++ link.sh
@@ -1,2 +1,5 @@
 ln -s ~/Code/code_profile/.vimrc ~/.vimrc
+ln -s ~/Code/code_profile/.zshrc ~/.zshrc
+ln -s ~/Code/code_profile/.zprofile ~/.zprofile
+ln -s ~/Code/code_profile/.tmux.conf ~/.tmux.conf
 
