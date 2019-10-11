3780022242a437be182999db85ae7c44642a1649
GeekerHua <geekerhua@sina.com>
2019-10-11 13:28:15 +0800

    update com.apple.AppleMultitouchTrackpad

--- mac_initial.sh
+++ mac_initial.sh
@@ -58,5 +58,12 @@ mac_setting() {
     defaults write com.apple.dock autohide-delay -int 0.5;killall Dock #（时间设为 0.5s）
     defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock # (去掉过渡动画)
     defaults delete com.apple.dock autohide-time-modifier;killall Dock # (恢复默认过渡动画)
+    
+    # touch
+    defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1;
+    defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0;
+    defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 0;
+    defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1;
+    
 }
 source ~/.zshrc
